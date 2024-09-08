9b9995d65f9564bf4647def07928caa56c07181f
Dimitris Kapanidis <d.kapanidis@gmail.com>
2024-09-08 17:49:10 +0200

    add trakt model

--- src/models/trakt.go
+++ src/models/trakt.go
@@ -7,21 +7,37 @@ type TraktMovie struct {
 }
 
 type IDs struct {
-	Trakt int    `json:"trakt"`
-	Slug  string `json:"slug"`
-	IMDB  string `json:"imdb"`
-	TMDB  int    `json:"tmdb"`
+	Trakt  int     `json:"trakt"`
+	Slug   string  `json:"slug"`
+	IMDB   string  `json:"imdb"`
+	TMDB   int     `json:"tmdb"`
+	TVDB   int     `json:"tvdb"`
+	TVRage *string `json:"tvrage"`
 }
 
 type WatchedItem struct {
-	WatchedAt string      `json:"watched_at"`
-	Type      string      `json:"type"`
-	Movie     *TraktMovie `json:"movie,omitempty"`
-	Show      *TraktShow  `json:"show,omitempty"`
+	Plays         int            `json:"plays"`
+	LastWatchedAt string         `json:"last_watched_at"`
+	LastUpdatedAt string         `json:"last_updated_at"`
+	ResetAt       *string        `json:"reset_at"`
+	Movie         *TraktMovie    `json:"movie,omitempty"`
+	Show          *TraktShow     `json:"show,omitempty"`
+	Seasons       *[]TraktSeason `json:"seasons,omitempty"`
 }
 
 type TraktShow struct {
 	Title string `json:"title"`
 	Year  int    `json:"year"`
 	IDs   IDs    `json:"ids"`
+}
+
+type TraktSeason struct {
+	Episodes []TraktEpisode `json:"episodes"`
+	Number   int            `json:"number"`
+}
+
+type TraktEpisode struct {
+	LastWatchedAt string `json:"last_watched_at"`
+	Number        int    `json:"number"`
+	Plays         int    `json:"plays"`
 }
